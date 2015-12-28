6ecfdd4f8cc1f71bc979f0f343248ea53dedabd5
Hans Åge Martinsen <hansm@hig.no>
2015-12-28 09:56:02 +0100

    Finished the menu class. The methods referenced by the menu class is still not done, still under work.

--- src/dmarcreport.py
+++ src/dmarcreport.py
@@ -1,3 +1,5 @@
+# -*- coding: latin-1 -*-
+
 ''' A simple Tkinter graphical user interface for reading DMARC XML files. '''
 
 import Tkinter as tk
@@ -6,22 +8,29 @@ import ttk
 
 class About(tk.Toplevel):
 
-    ''' DmarcParsers modal about window. '''
+    ''' DmarcReports modal about window. '''
 
     def __init__(self, master):
         # TODO: I have been trying to make this window completely modal, but is
-        # unable to do so. Will keep this TODO here to make it possible for me
-        # to fix this at a later time.
+        # unable to do so. Will keep this TODO here to remember me to fix this
+        # at a later time.
         tk.Toplevel.__init__(self, master)
         self.transient(master)
         self.master = master
-        self.title('About DmarcParser')
+        self.title('About DmarcReport')
+
+        self.aboutcontent = '''
+            Created by Hans Åge Martinsen
+            Email: <hamartin@moshwire.com>
+            URL: https://github.com/hamartin/DmarcReport
+        '''
 
         self.destroy = self.register(self.killwin)
         self.protocol('WM_DELETE_WINDOW', self.destroy)
 
         self.fr_body = ttk.Frame(self, padding=5)
-        self.la_about = ttk.Label(self.fr_body, text='About')
+        self.la_about = ttk.Label(self.fr_body, text=self.aboutcontent,
+                                  padding=(0, 0, 50, 0))
         self.bu_ok = ttk.Button(self.fr_body, text='OK', command=self.destroy)
         self.bu_ok.focus()
         self.bu_ok.bind('<Return>', self.destroy)
@@ -48,14 +57,22 @@ class DmarcReport(tk.Tk):
         self.title('DmarcReport')
 
         self.menu = Menu(self)
-        # TODO: Arguments for this call has to be changed into something that
-        # actually work as intended.
-        self.menu.init(self.quit)
+
+        self.fr_begin = ttk.Frame(self)
+        self.fr_begin.pack()
+        self.bu_quit = ttk.Button(self, text='Quit', command=self.quit)
+        self.bu_quit.pack()
+        self.bu_open = ttk.Button(self, text='Open', command=self.open)
+        self.bu_open.pack()
+
+    def open(self):
+        ''' foo '''
+        print "foo"
 
 
 class Menu(tk.Menu):
 
-    ''' DmarcParsers menu bar. '''
+    ''' DmarcReports menu bar. '''
 
     def __init__(self, master=None):
         tk.Menu.__init__(self, master)
@@ -64,9 +81,11 @@ class Menu(tk.Menu):
         self.me_file = tk.Menu(self, tearoff=0)
         self.me_about = tk.Menu(self, tearoff=0)
 
-    def init(self, openfile):
+        self.init()
+
+    def init(self):
         ''' Initializes the menu bar. '''
-        self.me_file.add_command(label='Open', command=openfile)
+        self.me_file.add_command(label='Open', command=self.master.open)
         self.me_file.add_separator()
         self.me_file.add_command(label='Quit', command=self.quit)
         self.add_cascade(label='File', menu=self.me_file)
