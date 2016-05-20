de6d2a5d49c38a237c2b7c4609e9ad244f05d47b
Hans Åge Martinsen <hamartin@moshwire.com>
2016-05-20 20:01:22 +0200

    Comitting to remove the files I wont be using in kivy.

--- src/config.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-'''Configuration for Dmarc Report.'''
-
-PROG_NAME = 'Dmarc Report'
--- src/exceptions.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-'''DmarcReport exceptions.'''
-
-
-class DmarcReportError(Exception):
-    '''DmarcReport general exception class.'''
-    pass
