eb424d74ca8c20074e8c7c4e95cc595922edbc05
Hans Åge Martinsen <hamartin@moshwire.com>
2016-05-29 09:46:28 +0200

    Revert "Made a lot of progress on making the display as I want it."
    This reverts commit e96d43b20e7b712c133189a4f6b997b6c91418e4.

--- src/config.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-'''Config settings for Dmarc Report.'''
-
-PROGRAM_NAME = 'Dmarc Report'
-KV_DIRECTORY = './src/kv'
--- src/dmarcreport.py
+++ src/dmarcreport.py
@@ -4,47 +4,12 @@
 
 import kivy
 kivy.require('1.9.1')
-import cProfile
 
 from kivy.app import App
-from kivy.base import EventLoop
-
-import src.config as cnf
-import src.dmarcfront as dmf
 
 
-class DmarcReport(App):
+class DmarcReportApp(App):
 
     '''Dmarc Parser.'''
 
-    def __init__(self, **kwargs):
-        super(DmarcReport, self).__init__(**kwargs)
-        self.kv_directory = cnf.KV_DIRECTORY
-        self.use_kivy_settings = False
-
-        # Variables used by the application itself.
-        self.profile = None
-
-    def build(self):
-        '''Builds Dmarc Report.'''
-        return dmf.DmarcReportFront()
-
-    def open_settings(self, *largs):
-        '''Handles the settings window.'''
-        # Hinder the settings window from opening at all.
-        pass
-
-    # Profiling stuff.
-    def on_start(self):
-        '''Does some initial setting of GUI. Also starts a cProfile session.'''
-        # Starting cProfile session.
-        self.profile = cProfile.Profile()
-        self.profile.enable()
-        # Setting new title.
-        EventLoop.window.title = cnf.PROGRAM_NAME
-
-    def on_stop(self):
-        '''Stops the running profiling session.'''
-        # Stopping the cProfile session.
-        self.profile.disable()
-        self.profile.dump_stats('{0}.profile'.format(cnf.PROGRAM_NAME))
+    pass
