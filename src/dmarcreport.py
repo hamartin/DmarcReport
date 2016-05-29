280c56a4c6a9aa9dfb5c0e2b75010d6d8f1aa3dd
Hans Åge Martinsen <hamartin@moshwire.com>
2016-05-29 10:00:09 +0200

    Revert "Revert "Adding the last commit for today!""
    This reverts commit 81b567ad710173abd453ac0928835f72e4e8cb46.

--- src/dmarcreport.py
+++ src/dmarcreport.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 # File name: dmarcreport.py
 
-'''Dmarc Report module'''
+'''Dmarc Report module.'''
 
 import kivy
 kivy.require('1.9.1')
@@ -9,9 +9,20 @@ kivy.require('1.9.1')
 from kivy.app import App
 
 from src import config as cnf
+from src.footer import Footer
 
 
 class DmarcReport(App):
 
-    '''Dmarc Report'''
-    kv_directory = cnf.KV_DIRECTORY
+    '''Dmarc Report.'''
+
+    def __init__(self, **kwargs):
+        super(DmarcReport, self).__init__(**kwargs)
+        self.kv_directory = cnf.KV_DIRECTORY
+        self.footer = Footer()
+
+        # Self is the view.
+        # Model.
+        # TODO: Put the model in here when you get that far.
+        # Controller.
+        # TODO: Put the controller in here when you get that far.
