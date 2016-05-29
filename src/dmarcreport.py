3c0e2998af178d2b97e877bd58150857f2d491cc
Hans Åge Martinsen <hamartin@moshwire.com>
2016-05-29 10:00:03 +0200

    Revert "Revert "Adding a lot of stuff before abandoning this branch""
    This reverts commit e6f6236ff7b6238f969a62be7f8e8db35b707086.

--- src/dmarcreport.py
+++ src/dmarcreport.py
@@ -7,12 +7,20 @@ import kivy
 kivy.require('1.9.1')
 
 from kivy.app import App
+from kivy.lang import Builder
+
 
 from src import config as cnf
+from src.footer import Footer
 
 
 class DmarcReport(App):
 
     '''Dmarc Report'''
 
-    kv_directory = cnf.KV_DIRECTORY
+    def __init__(self, **kwargs):
+        super(DmarcReport, self).__init__(**kwargs)
+        Builder.load_file('{0}/generic.kv'.format(cnf.KV_DIRECTORY))
+        Builder.load_file('{0}/dmarcreport.kv'.format(cnf.KV_DIRECTORY))
+
+        self.footer = Footer()
