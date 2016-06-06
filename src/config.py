43094e7e78f0628713ba11927e4463d8036c3c2b
Hans Åge Martinsen <hamartin@moshwire.com>
2016-06-06 11:04:58 +0200

    Adding copying permission in every file as explained on this site http://www.gnu.org/licenses/gpl-howto.html to make code GPL.

--- src/config.py
+++ src/config.py
@@ -5,6 +5,21 @@
 Dmarc Report config module.
 
 Copyright 2016, Hans Åge Martinsen <hamartin@moshwire.com>
+
+    This file is part of Dmarc Report.
+
+    Dmarc Report is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Dmarc Report is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Dmarc Report.  If not, see <http://www.gnu.org/licenses/>
 '''
 
 KV_DIRECTORY = 'kv'
